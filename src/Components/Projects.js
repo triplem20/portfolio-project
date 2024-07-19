95214b7fcf0daf533a67b0c15f134ea855600d93
mustafa <mustafastudies123@gmail.com>
2024-07-20 01:41:37 +0300

    20.7.24

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,74 +1,69 @@
-import { Col, Container, Row, Tab, Nav  } from "react-bootstrap";
-import { ProjectTile } from "./projectTile";
-import projimg1 from "../assets/img/projimg1.jpeg";
-import projimg2 from "../assets/img/projimg2.jpeg";
-import projimg3 from "../assets/img/projimg3.jpeg";
+import React from 'react';
+import { Col, Container, Row, Tab, Nav } from 'react-bootstrap';
+import { ProjectTile } from './projectTile';
+import projimg1 from '../assets/img/projimg1.jpeg';
+import projimg2 from '../assets/img/projimg2.jpeg';
+import projimg3 from '../assets/img/projimg3.jpeg';
 
-
-
-export const Projects= ()=>{
-
-const projects =[
+export const Projects = () => {
+  const projects = [
     {
-        title: "Cleaning Service Mobile App with Admin",
-        description: "A description",
-        imgUrl:projimg1,
+      title: 'Cleaning Service Mobile App with Admin',
+      description: 'A description',
+      imgUrl: projimg1,
     },
     {
-        title: "Project 2",
-        description: "A description",
-        imgUrl:projimg2,
+      title: 'Project 2',
+      description: 'A description',
+      imgUrl: projimg2,
     },
     {
-        title: "Project 3",
-        description: "A description",
-        imgUrl:projimg3,
+      title: 'Project 3',
+      description: 'A description',
+      imgUrl: projimg3,
     },
-];
+  ];
 
-    return(
-      <>
-      <div className='Container'>
-        <section className="project" id="project">
-            <Container>
-                <Row>
-                    
-                    <h1 className="ptitle">Projects</h1>
-                    <p>These are the projects I have worked on, some are still under development.<br></br> Please view them and provide some feedback.</p>
-                    
-                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content >
-                    <Tab.Pane >
-                      
-                      {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectTile
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-                </Row>
-            </Container>
-        </section>
-        </div>
-        </>
-    );
-  }
+  return (
+    <div className='Container'>
+      <section className='project' id='project'>
+        <Container>
+          <Row>
+            <Col xs={12}>
+              <h1 className='ptitle'>Projects</h1>
+              <p>
+                These are the projects I have worked on, some are still under development. Please view them and provide some feedback.
+              </p>
+            </Col>
+          </Row>
+          <Tab.Container id='projects-tabs' defaultActiveKey='first'>
+            <Nav variant='pills' className='nav-pills mb-5 justify-content-center'>
+              <Nav.Item>
+                <Nav.Link eventKey='first'>Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='second'>Tab 2</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='third'>Tab 3</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey='first'>
+                {projects.map((project, index) => (
+                  <ProjectTile
+                    key={index}
+                    title={project.title}
+                    description={project.description}
+                    imgUrl={project.imgUrl}
+                  />
+                ))}
+              </Tab.Pane>
+              {/* You can add more Tab.Pane components here for other tabs */}
+            </Tab.Content>
+          </Tab.Container>
+        </Container>
+      </section>
+    </div>
+  );
+};
--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,81 +1,89 @@
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'; // Ensure you have emailjs-com installed
 import { Link } from 'react-router-dom';
-import React from 'react';
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
 
-
-export const Contact= () => {
-
+export const Contact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm('service_hh41c0j', 'template_qd0izyj', form.current, {
-        publicKey:'7qAkaYaXqQ32B1M9R',
-      })
+    emailjs.sendForm('service_q2n4ogx', 'template_qd0izyj', form.current, 'user_GHD603Z-lPDtcT2Qp')
       .then(
-        () => {
-          console.log('SUCCESS!');
+        (result) => {
+          console.log('SUCCESS!', result.text);
+          alert('Your message has been sent successfully!');
+          form.current.reset(); // Reset form after successful submission
         },
         (error) => {
           console.log('FAILED...', error.text);
-        },
+          alert('There was an error sending your message. Please try again.');
+        }
       );
-  
+  };
 
-    };
+  return (
+    <>
+      <div className='Container'>
+        <h1 id="connect">Let's Connect!</h1>
 
+        <div className="containerc">
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="fname">Full Name:</label>
+              </div>
+              <div className="col-75">
+                <input
+                  type="text"
+                  id="fname"
+                  name="from_name"
+                  placeholder="Full Name"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="email">Email:</label>
+              </div>
+              <div className="col-75">
+                <input
+                  type="email"
+                  id="email"
+                  name="from_email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="subject">Request Description:</label>
+              </div>
+              <div className="col-75">
+                <textarea
+                  id="subject"
+                  name="description"
+                  placeholder="Write something..."
+                  required
+                ></textarea>
+              </div>
+            </div>
+            <br />
+            <div className="row">
+              <button type="submit">Send</button>
+            </div>
+          </form>
+        </div>
+      </div>
 
-
-    return(
-        <>
-        <div className='Container'>
-<h1 id="connect">Let's Connect!</h1>
-
-<div class="containerc">
- <form ref={form} onSubmit={sendEmail}>
-  <div class="row">
-    <div class="col-25">
-      <label for="fname">Full Name:</label>
-    </div>
-    <div class="col-75">
-      <input type="text" id="fname" name="from_name" placeholder="Full Name"/>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-25">
-      <label for="email">Email:</label>
-    </div>
-    <div class="col-75">
-      <input type="email" id="email" name="from_email" placeholder="Email"/>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-25">
-      <label for="subject">Request Description: </label>
-    </div>
-    <div class="col-75">
-      <textarea id="subject" name="description" placeholder="Write something.." ></textarea>
-    </div>
-  </div>
-  <br/>
-  <div class="row">
-    <input type="submit" value="Send"/>
-  </div>
-  </form>
-</div>
-</div>
-
-<nav className='footer-nav'>
-            <Link to='/' className="nav-item">Home</Link>
-            <Link to='About' className="nav-item">About</Link>
-            <Link to='projects' className="nav-item">Projects</Link>
-            <Link to='contact' className="nav-item">Contact</Link>
-            </nav>
-            
-            </>
-    );
+      <nav className='footer-nav'>
+        <Link to='/' className="nav-item">Home</Link>
+        <Link to='About' className="nav-item">About</Link>
+        <Link to='projects' className="nav-item">Projects</Link>
+        <Link to='contact' className="nav-item">Contact</Link>
+      </nav>
+    </>
+  );
 };
-
